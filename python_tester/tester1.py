b3fc12aac875b7f20c52a6f591aface6225ef285
Darja Potak <darjapotak248@gmail.com>
2024-11-06 14:14:09 +0200

    commit67

--- python_tester/tester1.py
+++ python_tester/tester1.py
@@ -1,8 +1,19 @@
 from bs4 import BeautifulSoup
 import unittest
 
-file_code = "C:\\Users\\darja\\OneDrive\\Desktop\\html_Progects\\HTML_projects\\HTML_Portfolio2_official\\Portfolio.html"
-with open (file_code, "r", encoding="utf-8") as file:
-    html_code = file.read()
+class TestContentHTMLCode(unittest.TestCase):
+    #
+    file_code = "C:\\Users\\darja\\OneDrive\\Desktop\\html_Progects\\HTML_projects\\HTML_Portfolio2_official\\Portfolio.html"
+    with open (file_code, "r", encoding="utf-8") as file:
+        html_code = file.read()
+        
+    soup = BeautifulSoup(html_code, "html.parser")
 
-soup = BeautifulSoup(html_code, "html.parser")
+    def title_test(self):
+        title = self.soup.title
+        self.assertIsNotNone(title)
+        self.assertEqual(title.string, "Test Page")
+
+
+if __name__ == "__main__":
+    unittest.main()
