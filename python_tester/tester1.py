7df89dae63ebdde7217455d22fd10946022351d9
Darja Potak <darjapotak248@gmail.com>
2024-11-06 14:53:14 +0200

    commit69

--- python_tester/tester1.py
+++ python_tester/tester1.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Tester HTML
 class TestContentHTMLCode(unittest.TestCase):
@@ -22,6 +23,9 @@ class TestContentCSSCode(unittest.TestCase):
     file_code_css = "C:\\Users\\darja\\OneDrive\\Desktop\\html_Progects\\HTML_projects\\HTML_Portfolio2_official\\css_prog\\portfolio_1.css"
     with open (file_code_css, "r") as file_css:
         css_code = file_css.read()
+
+    driver = webdriver.Chrome()
+    driver.get("C:\\Users\\darja\\OneDrive\\Desktop\\html_Progects\\HTML_projects\\HTML_Portfolio2_official\\css_prog\\portfolio_1.css")
 
     def css_test1():
         pass
